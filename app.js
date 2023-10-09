d4d40d4e6d171c00c9e6e36693bcf734515f0420
Parth Nikam <parthsnikam12co@gmail.com>
2023-10-09 18:13:13 +0530

    .env / .gitignore

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
