53d0722ec519d580ae913ce7ba2a0d5377383838
Parth Nikam <parthsnikam12co@gmail.com>
2023-10-09 10:27:57 +0530

    Level 1 Authentication : - Username & Password

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
+// jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,18 +13,60 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function (req, res) {
     res.render("home");
-})
+});
 
 app.get("/login", function (req, res) {
     res.render("login");
-})
+});
 
 app.get("/register", function (req, res) {
     res.render("register");
+});
+
+app.post("/register", async function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    try {
+        await newUser.save();
+        res.render("secrets");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post("/login", async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
 })
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
-})
+});
