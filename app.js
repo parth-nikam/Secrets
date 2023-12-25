775d6b1531c8e705200ea518d786eb9fe46cfb28
Parth Nikam <parthsnikam12co@gmail.com>
2023-12-25 11:09:33 +0530

    test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const encrypt = require("mongoose-encryption");
 
 const app = express();
 
+console.log(process.env.SECRET)
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
